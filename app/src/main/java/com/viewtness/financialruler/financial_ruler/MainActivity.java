2fd3c779f683624f656a5fcb7c48f17c39eaa47b
wjx367 <646432904@qq.com>
2017-09-25 15:29:55 +0800

    添加 尺子 可设置 刻度的最大值和最小值 最小刻度  开始定位到指点的位置

--- app/src/main/java/com/viewtness/financialruler/financial_ruler/MainActivity.java
+++ app/src/main/java/com/viewtness/financialruler/financial_ruler/MainActivity.java
@@ -2,12 +2,45 @@ package com.viewtness.financialruler.financial_ruler;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.viewtness.financialruler.financial_ruler.RulerView.OnRulerChangeListener;
+import com.viewtness.financialruler.financial_ruler.RulerView.RulerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RulerView rulerView;
+    private TextView tvValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        initView();
+        initListener();
+    }
+
+    private void initView() {
+        rulerView = (RulerView) findViewById(R.id.rulerView);
+        tvValue = (TextView) findViewById(R.id.tv_value);
+    }
+    private void initListener() {
+        rulerView.setOnRulerChangeListener(new OnRulerChangeListener() {
+            @Override
+            public void onChanged(int newValue) {
+                tvValue.setText(newValue+"");
+            }
+        });
+
+        rulerView.setMaxValue(100000);
+        rulerView.setMinValue(0);
+        rulerView.setOneItemValue(20000);
+        rulerView.setOneItemValue(1000);
+        tvValue.setText(20000+"");
     }
 }
---
+++ app/src/main/java/com/viewtness/financialruler/financial_ruler/RulerView/OnRulerChangeListener.java
@@ -0,0 +1,15 @@
+package com.viewtness.financialruler.financial_ruler.RulerView;
+
+/**
+ * Title:RulerView
+ * Package:com.viewtness.financialruler.financial_ruler.RulerView
+ * Description:TODO
+ * Author: viewtness
+ * Date: 2017/9/25
+ * Version: V1.0.0
+ */
+
+public interface OnRulerChangeListener {
+
+    void onChanged(int newValue);
+}
